d446973e9da0c54e1a2ab2c1aa2f2fc23c3ac28d
cucumber229 <mavlav2005@gmail.com>
2024-07-02 16:19:33 +0300

    добавил таб бар

---
+++ hw2/Library.swift
@@ -0,0 +1,29 @@
+//
+//  Library.swift
+//  hw2
+//
+//  Created by Артур Мавликаев on 02.07.2024.
+//
+
+import UIKit
+
+class Library: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
